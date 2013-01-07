09f5b31d919a02157f39f19f568281671a89bf42
Benoit Myard <myardbenoit@gmail.com>
2013-01-07 09:31:43 +0700

    Added configuration for fish (based on bash)
    I guess a way to synchronize those two configs should be found,
    somehow.

---
+++ fish/config.fish
@@ -0,0 +1,39 @@
+#
+# Init file for fish
+#
+
+function fish_prompt
+    set_color -ob black yellow
+    echo -n (pwd|sed 's/.*.\///')
+
+    set_color normal
+    echo -n ' '
+
+    set_color red
+    hg prompt '{status}{branch}' 2>/dev/null
+
+    set_color normal
+    echo '% '
+end
+
+set -x EDITOR vim
+set -x VISUAL vim
+
+# Python Google App Engine root directory
+set -x PY_GAE_ROOT /home/benoit/vendor/gae-py/
+
+# Go variables needed for correct execution
+set -x GOROOT $HOME/vendor/go
+set -x PATH $PATH $GOROOT/bin
+
+# Nodejs and NPM should be in the path
+set -x PATH $PATH $HOME/vendor/node/bin
+set -x PATH $PATH $HOME/vendor/leiningen
+set -x PATH $PATH $HOME/vendor/iReport-3.7.6/bin
+
+set -x PATH $PATH ./node_modules/.bin
+
+# Add ~/bin to the path; comes last as to not shadow any system-wide command
+set -x PATH $PATH ~/bin
+
+set -x TZ Asia/Bangkok
