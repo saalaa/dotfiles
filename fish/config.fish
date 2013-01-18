9e25a37f71040184cf0105a0f7e59f8f4626dfc5
Benoit Myard <myardbenoit@gmail.com>
2013-01-18 22:16:40 +0700

    Improved PATH handling (got broken, somehow)

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
-#
 # Init file for fish
+# ==================
 #
-
 # The hg-prompt command can be found at https://bitbucket.org/sjl/hg-prompt
+#
 
 set fish_greeting ""
 
@@ -28,24 +28,22 @@ function fish_prompt
     echo '% '
 end
 
+set -x TZ Asia/Bangkok
+
 set -x EDITOR vim
 set -x VISUAL vim
 
+set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin ~/bin
+
 # Python Google App Engine root directory
-set -x PY_GAE_ROOT /home/benoit/vendor/gae-py/
+#set -x PY_GAE_ROOT /home/benoit/vendor/gae-py/
 
 # Go variables needed for correct execution
-set -x GOROOT $HOME/vendor/go
-set -x PATH $PATH $GOROOT/bin
+#set -x GOROOT $HOME/vendor/go
+#set -x PATH $PATH $GOROOT/bin
 
 # Nodejs and NPM should be in the path
-set -x PATH $PATH $HOME/vendor/node/bin
-set -x PATH $PATH $HOME/vendor/leiningen
-set -x PATH $PATH $HOME/vendor/iReport-3.7.6/bin
+#set -x PATH $PATH $HOME/vendor/node/bin
+#set -x PATH $PATH $HOME/vendor/leiningen
+#set -x PATH $PATH $HOME/vendor/iReport-3.7.6/bin
 
-set -x PATH $PATH ./node_modules/.bin
-
-# Add ~/bin to the path; comes last as to not shadow any system-wide command
-set -x PATH $PATH ~/bin
-
-set -x TZ Asia/Bangkok
