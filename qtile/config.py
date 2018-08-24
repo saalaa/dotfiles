c21e094027a0d30bf899978177a55636735e1a39
Benoit Myard <myardbenoit@gmail.com>
2018-08-24 17:22:35 +0200

    Update Qtile configuration

--- qtile/config.py
+++ qtile/config.py
@@ -6,55 +6,49 @@ from libqtile.config import (
     Key, Screen, Group, Drag, Click
 )
 
-mod = "mod1"
+mod = 'mod1'
 
 keys = [
-    Key([mod], "Return", lazy.spawn("gnome-terminal")),
-
-    # Lock screen
-    Key([mod, "control"], "l", lazy.spawn("slock")),
+    Key([mod], 'Return', lazy.spawn('gnome-terminal')),
 
     # Launch arbitrary commands
-    Key([mod], "p", lazy.spawncmd()),
-
-    # Cycle through configured layouts
-    Key([mod], "space", lazy.next_layout()),
+    Key([mod], 'p', lazy.spawncmd()),
 
     # Toggle between maximized and tiled (in stack)
-    Key([mod, "shift"], "space", lazy.layout.toggle_split()),
+    Key([mod], 't', lazy.layout.toggle_split()),
 
-    # Focus clients through movements
-    Key([mod], "k", lazy.layout.up()),
-    Key([mod], "j", lazy.layout.down()),
-    Key([mod], "h", lazy.layout.left()),
-    Key([mod], "l", lazy.layout.right()),
-
-    # Move clients through movements
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+    # Cycle through configured layouts
+    Key([mod], 'space', lazy.next_layout()),
 
     # Manage floating clients
-    Key([mod], "f", lazy.window.toggle_floating()),
+    Key([mod, 'shift'], 'space', lazy.window.toggle_floating()),
 
-    # Key([mod], "f", lazy.window.toggle_fullscreen()),
-    # Key([mod], "m", lazy.window.toggle_minimize()),
+    # Focus clients through movements
+    Key([mod], 'k', lazy.layout.up()),
+    Key([mod], 'j', lazy.layout.down()),
+    Key([mod], 'h', lazy.layout.left()),
+    Key([mod], 'l', lazy.layout.right()),
 
-    # Swap panes of split stack
-    # Key([mod, "shift"], "space", lazy.layout.rotate()),
+    # Move clients through movements
+    Key([mod, 'shift'], 'j', lazy.layout.shuffle_down()),
+    Key([mod, 'shift'], 'k', lazy.layout.shuffle_up()),
+    Key([mod, 'shift'], 'h', lazy.layout.shuffle_left()),
+    Key([mod, 'shift'], 'l', lazy.layout.shuffle_right()),
+
+    # Lock screen
+    # Key([mod, 'shift'], 'l', lazy.spawn('slock')),
 
     # Kill focused client
-    Key([mod, "control"], "w", lazy.window.kill()),
+    Key([mod, 'shift'], 'w', lazy.window.kill()),
 
     # Restart window manager
-    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, 'shift'], 'r', lazy.restart()),
 
     # Kill window manager
-    Key([mod, "control"], "q", lazy.shutdown()),
+    Key([mod, 'shift'], 'q', lazy.shutdown()),
 ]
 
-groups = [Group(i) for i in "123456"]
+groups = [Group(i) for i in 'asdf']
 
 for i in groups:
     keys.extend([
@@ -62,13 +56,15 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen()),
 
         # mod1 + shift + letter = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)),
     ])
 
 layouts = [
-    layout.Floating(name='f'),
     layout.Wmii(
-        name='w', border_width=1, margin=5
+        name='@wmii', border_width=1, margin=5
+    ),
+    layout.Floating(
+        name='@floating'
     )
 ]
 
@@ -85,25 +81,20 @@ screens = [
         bottom=bar.Bar(
             [
                 widget.Wallpaper(
-                    label='@',
+                    label='%',
                     random_selection=True,
                     directory=os.path.expanduser(
                         '~/Pictures/wallpapers/'
                     )
                 ),
                 widget.CurrentLayout(),
-                # widget.GroupBox(),
+                widget.GroupBox(),
                 widget.Prompt(),
                 widget.Spacer(),
-                widget.MemoryGraph(
-                    border_width=0, line_width=1
-                ),
-                widget.CPUGraph(
-                    border_width=0, line_width=1
-                ),
                 # widget.Systray(),
+                widget.Pomodoro(),
                 widget.Clock(
-                    format='%Y-%m-%d %H:%m'
+                    format='%Y-%m-%d %H:%M'
                 )
             ],
             24
@@ -113,11 +104,11 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], 'Button2', lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
@@ -150,7 +141,7 @@ floating_layout = layout.Floating(float_rules=[
 
 auto_fullscreen = True
 
-focus_on_window_activation = "smart"
+focus_on_window_activation = 'smart'
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -160,4 +151,4 @@ focus_on_window_activation = "smart"
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+wmname = 'LG3D'
